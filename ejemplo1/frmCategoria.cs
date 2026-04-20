4e8026e808c73dd72ba508ef480aef09a6c14bc0
Santiago Lapeña <slapena88@gmail.com>
2026-04-20 19:46:38 +0200

    listado de categoria con al ventana

--- ejemplo1/frmCategoria.cs
+++ ejemplo1/frmCategoria.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using dominio;
+using negocio;
 
 namespace ejemplo1
 {
     public partial class frmCategoria : Form
     {
+        List<Categoria> catLista = new List<Categoria>();
         public frmCategoria()
         {
             InitializeComponent();
@@ -19,7 +22,19 @@ namespace ejemplo1
 
         private void frmCategoria_Load(object sender, EventArgs e)
         {
+            CategoriaNegocio catNegocio = new CategoriaNegocio();
 
+            try
+            {
+                catLista = catNegocio.listar();
+                dgwCategoria.DataSource = catLista;
+                
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
         }
     }
 }
--- negocio/CategoriaNegocio.cs
+++ negocio/CategoriaNegocio.cs
@@ -1,13 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using datos;
+using dominio;
 
 namespace negocio
 {
-    internal class CategoriaNegocio
+    public class CategoriaNegocio
     {
-        
+        List<Categoria> lista = new List<Categoria>();
+        AccesoDatos datos = new AccesoDatos();
+        public List<Categoria> listar()
+        {
+            try
+            {
+                datos.setQuery("select id, descripcion from CATEGORIAS");
+                datos.lecturaDatos();
+
+                while (datos.Lector.Read())
+                {
+                    Categoria aux = new Categoria();
+                    aux.Id = (int)datos.Lector["id"];
+                    aux.Nombre = (string)datos.Lector["descripcion"];
+
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
     }
 }
